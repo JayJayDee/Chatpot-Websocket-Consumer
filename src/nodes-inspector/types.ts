b2306bf782482a60ebba8d48e75ece0025fdb4af
JayJayDee <jindongp@gmail.com>
2019-05-03 17:33:46 +0900

    added implementation for nodes-inspector.

--- src/nodes-inspector/types.ts
+++ src/nodes-inspector/types.ts
@@ -1,3 +1,12 @@
 export namespace NodesInspectorTypes {
+  export type NodeStatus = {
+    publicHost: string;
+    privateHost: string;
+    port: number;
+    numClient: number;
+  };
 
+  export type Inspect = () => Promise<void>;
+  export type ReportAlive = (status: NodeStatus) => Promise<void>;
+  export type PickHealthyNode = () => Promise<NodeStatus>;
 }
