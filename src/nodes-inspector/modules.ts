cd3cf4c789c92f88ebd8f32217465c6e048d3874
JayJayDee <jindongp@gmail.com>
2019-05-05 19:56:51 +0900

    added nodes-inspector::updateReport()

--- src/nodes-inspector/modules.ts
+++ src/nodes-inspector/modules.ts
@@ -1,5 +1,6 @@
 export namespace NodesInspectorModules {
   export const ReportAlive = 'NodesInspector/ReportAlive';
+  export const UpdateReport = 'NodesInspector/UpdateReport';
   export const Inspect = 'NodesInspector/Inspect';
   export const InspectionRunner = 'NodesInspector/InspectionRunner';
   export const PickHealthyNode = 'NodesInspector/PickHealthyNode';
