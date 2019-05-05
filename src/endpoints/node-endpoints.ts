7eae9acffd87cbd7b02ca9e6862f58d03dc606f5
JayJayDee <jindongp@gmail.com>
2019-05-05 09:43:24 +0900

    added readme added implementation for GET /status endpoint.

--- src/endpoints/node-endpoints.ts
+++ src/endpoints/node-endpoints.ts
@@ -1,15 +1,22 @@
 import { injectable } from 'smart-factory';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
+import { NodesInspectorModules, NodesInspectorTypes } from '../nodes-inspector';
 
 injectable(EndpointModules.Node.Status,
-  [ EndpointModules.Utils.WrapAync ],
-  async (wrapAsync: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> => ({
-    uri: '/status',
-    method: EndpointTypes.EndpointMethod.GET,
-    handler: [
-      wrapAsync(async (req, res, next) => {
-        res.status(200).json({});
-      })
-    ]
-  }));
+  [ EndpointModules.Utils.WrapAync,
+    NodesInspectorModules.PickHealthyNode ],
+  async (wrapAsync: EndpointTypes.Utils.WrapAsync,
+    pickHealthyNode: NodesInspectorTypes.PickHealthyNode): Promise<EndpointTypes.Endpoint> =>
+
+    ({
+      uri: '/status',
+      method: EndpointTypes.EndpointMethod.GET,
+      handler: [
+        wrapAsync(async (req, res, next) => {
+          const node = await pickHealthyNode();
+          console.log(node);
+          res.status(200).json({});
+        })
+      ]
+    }));
