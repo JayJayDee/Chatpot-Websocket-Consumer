81b56bd1021d15e0fd48a186a58f450ce1f14a53
JayJayDee <jindongp@gmail.com>
2019-05-05 20:12:44 +0900

    added node status update routine to websocket-runner

--- src/websockets/types.ts
+++ src/websockets/types.ts
@@ -2,10 +2,12 @@ import { Socket } from 'socket.io';
 
 export namespace WebsocketTypes {
   type SocketCallback = (socket: Socket) => void;
+  type ClientsCallback = (err: Error, clients: string[]) => void;
 
   export type WebsocketWrap = {
     on: (event: string, callback: SocketCallback) => void;
     listen: (port: number) => void;
+    clients: (callback: ClientsCallback) => void;
   };
 
   export type WebsocketInstantiator = () => Promise<WebsocketWrap>;
