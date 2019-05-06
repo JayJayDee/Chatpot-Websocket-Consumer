b576f82b205c309cb75a4b1f57faf8c67615ae48
JayJayDee <jindongp@gmail.com>
2019-05-06 20:27:38 +0900

    added inspect() function implementation.

--- src/nodes-inspector/node-alive-inspector.ts
+++ src/nodes-inspector/node-alive-inspector.ts
@@ -2,16 +2,26 @@ import { injectable } from 'smart-factory';
 import { NodesInspectorModules } from './modules';
 import { NodesInspectorTypes } from './types';
 import { LoggerModules, LoggerTypes } from '../loggers';
+import { KeyValueStorageModules, KeyValueStorageTypes } from '../kv-storage';
 
 const tag = '[ws-node-inspector]';
+const listKey = 'WS_NODES';
 
 injectable(NodesInspectorModules.Inspect,
-  [ LoggerModules.Logger ],
-  async (log: LoggerTypes.Logger): Promise<NodesInspectorTypes.Inspect> =>
+  [ LoggerModules.Logger,
+    KeyValueStorageModules.GetRedisClient ],
+  async (log: LoggerTypes.Logger,
+    getRedisClient: KeyValueStorageTypes.GetRedisClient): Promise<NodesInspectorTypes.Inspect> =>
 
-    async () => {
-
-    });
+    () =>
+      new Promise((resolve, reject) => {
+        getRedisClient().then((client) => {
+          client.lrange(listKey, 0, 100, (err, replies) => {
+            if (err) return reject(err);
+            console.log(replies.length);
+          });
+        });
+      }));
 
 
 injectable(NodesInspectorModules.InspectionRunner,
