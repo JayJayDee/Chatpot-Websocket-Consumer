d15e2a31b52bc226b1f6d29b697c46b904fbb5fc
JayJayDee <jindongp@gmail.com>
2019-05-11 18:21:58 +0900

    added redis-socket-mapper implementations.

--- src/socket-mapper/redis-socket-mapper.ts
+++ src/socket-mapper/redis-socket-mapper.ts
@@ -2,16 +2,48 @@ import { LoggerTypes } from '../loggers';
 import { KeyValueStorageTypes } from '../kv-storage';
 import { SocketMapperTypes } from './types';
 
+const fromTokenToSocket = (memberToken: string) =>
+ `MAP_T2S_${memberToken}`;
+
+const fromSocketToToken = (socketId: string) =>
+ `MAP_S2T_${socketId}`;
+
 export default (log: LoggerTypes.Logger,
     getRedisClient: KeyValueStorageTypes.GetRedisClient): SocketMapperTypes.SocketMapper => {
 
       return {
-        async map(socketId, memberToken) {
+        map: (socketId, memberToken) =>
+          new Promise(async (resolve, reject) => {
+            const client = await getRedisClient();
+            client.multi()
+              .set(fromTokenToSocket(memberToken), socketId)
+              .set(fromSocketToToken(socketId), memberToken)
+              .exec((err, reply) => {
+                if (err) return reject(err);
+                resolve();
+              });
+          }),
 
-        },
-        async unmap(socketId, memberToken) {
+        unmap: (param) =>
+          new Promise(async (resolve, reject) => {
+            const client = await getRedisClient();
+            if (param.memberToken) {
+              client.get(fromTokenToSocket(param.memberToken), (err, socketId) => {
+                if (err) return reject(err);
+                client.del(fromTokenToSocket(param.memberToken));
+                client.del(fromSocketToToken(socketId));
+                resolve();
+              });
+            } else if (param.socketId) {
+              client.get(fromSocketToToken(param.socketId), (err, memberToken) => {
+                if (err) return reject(err);
+                client.del(fromSocketToToken(param.socketId));
+                client.del(fromTokenToSocket(memberToken));
+                resolve();
+              });
+            }
+          }),
 
-        },
         async fetchSocketId(memberToken: string) {
           return '';
         },
--- src/socket-mapper/types.ts
+++ src/socket-mapper/types.ts
@@ -1,6 +1,11 @@
 export namespace SocketMapperTypes {
+  type UnmapParam = {
+    socketId?: string;
+    memberToken?: string;
+  };
+
   export type MapSocket = (socketId: string, memberToken: string) => Promise<void>;
-  export type UnmapSocket = (socketId: string, memberToken: string) => Promise<void>;
+  export type UnmapSocket = (param: UnmapParam) => Promise<void>;
   export type FetchSocketId = (memberToken: string) => Promise<string>;
   export type FetchMemberToken = (socketId: string) => Promise<string>;
 
