4a5933e13582bec39701810493cbbf4c7402cb3f
JayJayDee <jindongp@gmail.com>
2019-05-15 14:27:21 +0900

    updated swagger-apidoc

--- src/swagger/swagger-ui-bootstrap.ts
+++ src/swagger/swagger-ui-bootstrap.ts
@@ -7,7 +7,7 @@ const apiDocOptions = {
   swaggerDefinition: {
     info: {
       title: 'Chatpot-Websocket-API',
-      description: 'Chatpot websocket cluster loadbalacing APIs',
+      description: 'Chatpot websocket cluster loadbalacing APIs <br /><br />Socket.IO 통신 프로토콜 문서는 <a href="http://dev-websocket.chatpot.chat/socketio-doc">다음</a>을 참조하도록 한다.',
       version: '1.0.0'
     }
   },
